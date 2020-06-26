f5b279a5d6f9f5e5f60b7d36c5cc8d2d118d5e8f
ahmedhdeawy <ahmedhdeawy@gmail.com>
2020-06-27 00:16:40 +0200

    disable zoom on mobile

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset='UTF-8'>
     <meta name='theme-color' content='#363062'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
 
     @yield('metatag')
 
