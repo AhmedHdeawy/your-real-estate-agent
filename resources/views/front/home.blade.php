4165de61e683538fc2a398fadd9195819ebff7de
ahmedhdeawy <ahmedhdeawy@gmail.com>
2020-11-30 23:22:57 +0200

    Fix issue

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -36,7 +36,7 @@
 							<div class='article-data'>
 								<h5> {{ $blog->title }} </h5>
 								<p>
-                                    {{ $blog->content }}
+                                    {!! substr($blog->content, 0, 200) !!}
                                 </p>
                             <a class='btn'
                                 href='{{ route('blog', $blog->id . '-' . make_slug($blog->title)) }}'> {{ __('lang.read_more') }} </a>
