522ac342e80dc60b5a9ee38de247afd0215d3e8c
ahmedhdeawy <ahmedhdeawy@gmail.com>
2020-12-01 01:55:56 +0200

    Enhance for testing

--- resources/views/front/propertyDetails.blade.php
+++ resources/views/front/propertyDetails.blade.php
@@ -9,7 +9,7 @@
 <main class='unit-page'>
     <div class='container'>
 
-        <div class="row">
+        <div class="row justify-content-center">
             <div class="col-12 col-md-10">
 
                 {{-- Property Images --}}
